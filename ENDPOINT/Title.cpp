328a440f2806a6c6b650d1405cb9686490d6e563
koko_MiiK <komyat.8mk@gmail.com>
2023-01-13 15:49:13 +0900

    メニュー追加

--- ENDPOINT/Title.cpp
+++ ENDPOINT/Title.cpp
@@ -7,16 +7,116 @@ Title::~Title()
 
 void Title::Initialize()
 {
-	
 }
 
 void Title::update()
 {
+	if (titleNameFont_NewRect.leftClicked()) { changeScene(SceneName::Stage1); }
+
+	if (KeyEnter.down() || KeyZ.down())
+	{
+		switch (menuID)
+		{
+		case MENU::New:
+			changeScene(SceneName::Stage1);
+			break;
+		default:
+			break;
+		}
+	}
+
+	if (KeyDown.down() || KeyS.down())
+	{
+		menuID += 1;
+		if (menuID > MENU::End)
+		{
+			menuID = MENU::New;
+		}
 	
+	}
+
+	if (KeyUp.down()|| KeyW.down())
+	{
+		menuID -= 1;
+		if (menuID < MENU::New)
+		{
+			menuID = MENU::End;
+		}
+	}
+
 }
 
 
 void Title::draw() const
 {
-	titleNameFont_GameTitle(U"E N D POINT").drawAt(TitleNameFontpos, Palette::Orange);
+	Color FontColor;
+	bool ColorFlag = false;
+
+
+	titleNameFont_NewRect.draw			(ColorF(1.0, 0.0, 0.0, 0.0));
+	titleNameFont_ContinueRect.draw		(ColorF(1.0, 0.0, 0.0, 0.0));
+	titleNameFont_SettingRect.draw		(ColorF(1.0, 0.0, 0.0, 0.0));
+	titleNameFont_EndRect.draw			(ColorF(1.0, 0.0, 0.0, 0.0));
+
+	titleNameFont_GameTitle(U"E N D POINT").drawAt(Scene::Center().x,100, Palette::Orange);
+
+	titleNameFont_NewRect.x = Scene::Center().x - (int)(titleNameFont_New(Menu_New).region().w / 2);
+
+	FontColor = titleNameFont_NewRect.mouseOver() ? Palette::Orange : Palette::White;
+	if (FontColor == Palette::Orange) { ColorFlag = true; }
+	titleNameFont_New(Menu_New).
+		draw(titleNameFont_NewRect.x, titleNameFont_NewRect.y, FontColor);
+
+	FontColor = titleNameFont_ContinueRect.mouseOver() ? Palette::Orange : Palette::White;
+	if (FontColor == Palette::Orange) { ColorFlag = true; }
+	titleNameFont_Continue(Menu_Continue).
+		draw(titleNameFont_ContinueRect.x, titleNameFont_ContinueRect.y, FontColor);
+
+	FontColor = titleNameFont_SettingRect.mouseOver() ? Palette::Orange : Palette::White;
+	if (FontColor == Palette::Orange) { ColorFlag = true; }
+	titleNameFont_Setting(Menu_Setting).
+		draw(titleNameFont_SettingRect.x, titleNameFont_SettingRect.y, FontColor);
+
+	FontColor = titleNameFont_EndRect.mouseOver() ? Palette::Orange : Palette::White;
+	if (FontColor == Palette::Orange) { ColorFlag = true; }
+	titleNameFont_End(Menu_End).
+		draw(titleNameFont_EndRect.x, titleNameFont_EndRect.y, FontColor);
+	
+
+
+
+	if (ColorFlag == false)
+	{
+		switch (menuID)
+		{
+		case MENU::Null:
+			break;
+		case MENU::New:
+			titleNameFont_New(U"始めから").
+				draw(titleNameFont_NewRect.x, titleNameFont_NewRect.y, Palette::Orange);
+			break;
+		case MENU::Continue:
+			titleNameFont_Continue(U"つづきから").
+				draw(titleNameFont_ContinueRect.x, titleNameFont_ContinueRect.y, Palette::Orange);
+			break;
+		case MENU::Setting:
+			titleNameFont_Setting(U"設定").
+				draw(titleNameFont_SettingRect.x, titleNameFont_SettingRect.y, Palette::Orange);
+			break;
+		case MENU::End:
+			titleNameFont_End(U"終わる").
+				draw(titleNameFont_EndRect.x, titleNameFont_EndRect.y, Palette::Orange);
+			break;
+		default:
+			break;
+		}
+	}
+
+
+
+
+
 }
+
+
+
--- ENDPOINT/Title.h
+++ ENDPOINT/Title.h
@@ -7,15 +7,61 @@ private:
 
 public:
 
-	Font titleNameFont_GameTitle{ 70,U"素材/6.font/jfdotfont-20150527/JF-Dot-ShinonomeMin12.ttf" };
-	Font titleNameFont_New{ 50,U"素材/6.font/jfdotfont-20150527/JF-Dot-ShinonomeMin12.ttf" };
-	Font titleNameFont_Continue{ 50,U"素材/6.font/jfdotfont-20150527/JF-Dot-ShinonomeMin12.ttf" };
-	Font titleNameFont_Setting{ 50,U"素材/6.font/jfdotfont-20150527/JF-Dot-ShinonomeMin12.ttf" };
-	Font titleNameFont_End{ 50,U"素材/6.font/jfdotfont-20150527/JF-Dot-ShinonomeMin12.ttf" };
+	String Menu_New = U"始めから";
+	String Menu_Continue = U"つづきから";
+	String Menu_Setting = U"設定";
+	String Menu_End = U"終わる";
 
-	Vec2 TitleNameFontpos{ 640,100 };
 
-	Rect titleNameFont_NewRect{};
+	Font titleNameFont_GameTitle	{ 70,U"素材/6.font/jfdotfont-20150527/JF-Dot-ShinonomeMin12.ttf" };
+	Font titleNameFont_New			{ 30,U"素材/6.font/jfdotfont-20150527/JF-Dot-ShinonomeMin12.ttf" };
+	Font titleNameFont_Continue		{ 30,U"素材/6.font/jfdotfont-20150527/JF-Dot-ShinonomeMin12.ttf" };
+	Font titleNameFont_Setting		{ 30,U"素材/6.font/jfdotfont-20150527/JF-Dot-ShinonomeMin12.ttf" };
+	Font titleNameFont_End			{ 30,U"素材/6.font/jfdotfont-20150527/JF-Dot-ShinonomeMin12.ttf" };
+
+	//Vec2 TitleNameFontpos{ 640,100 };
+
+	const int rectHeight = 30;
+
+	const int newrectWidth			= (const int)titleNameFont_New(Menu_New).region().w;
+	const int continuerectWidth		= (const int)titleNameFont_Continue(Menu_Continue).region().w;
+	const int settingrectWidth		= (const int)titleNameFont_Setting(Menu_Setting).region().w;
+	const int endrectWidth			= (const int)titleNameFont_End(Menu_End).region().w;
+
+
+	const int newRectX				= (const int)(Scene::Center().x - titleNameFont_New(Menu_New).region().w / 2);
+	const int ContinueRectX			= (const int)(Scene::Center().x - titleNameFont_Continue(Menu_Continue).region().w / 2);
+	const int SettingRectX			= (const int)(Scene::Center().x - titleNameFont_Setting(Menu_Setting).region().w / 2);
+	const int EndRectX				= (const int)(Scene::Center().x - titleNameFont_End(Menu_End).region().w / 2);
+	
+	
+
+	mutable Rect titleNameFont_NewRect		{newRectX,		290,	newrectWidth,		rectHeight};
+	mutable Rect titleNameFont_ContinueRect	{ContinueRectX,	340,	continuerectWidth,	rectHeight};
+	mutable Rect titleNameFont_SettingRect	{SettingRectX,	385,	settingrectWidth,	rectHeight};
+	mutable Rect titleNameFont_EndRect		{EndRectX,		440,	endrectWidth,		rectHeight};
+	/*
+	mutable Rect titleNameFont_NewRect		{TitleNameFontpos.x - 75,TitleNameFontpos.y + 190,140,30};
+	mutable Rect titleNameFont_ContinueRect	{TitleNameFontpos.x - 90,TitleNameFontpos.y + 240,170,30};
+	mutable Rect titleNameFont_SettingRect	{TitleNameFontpos.x - 30,TitleNameFontpos.y + 285,60,30};
+	mutable Rect titleNameFont_EndRect		{TitleNameFontpos.x - 45,TitleNameFontpos.y + 340,85,30};
+	*/
+
+
+
+	enum MENU
+	{
+		Null,
+		New,
+		Continue,
+		Setting,
+		End
+	};
+
+	int menuID = 0;
+
+
+
 
     Title(const InitData& init)
         : IScene(init)
