3348aec8138dc000fb11faed9db6f6746449cb7c
paper81 <114891681+paper81@users.noreply.github.com>
2023-01-24 16:08:58 +0900

    map関連

---
+++ ENDPOINT/App/Material/3.MAP/MapChip.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.2" name="MapChip" tilewidth="16" tileheight="16" tilecount="384" columns="24">
+ <image source="4.マップチップ.png" width="384" height="256"/>
+</tileset>
